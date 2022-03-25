367c9f2d9c095c2c2033e42fdf4e37fbc6bd18fd
Hadi Keramati <37190613+hkeramat@users.noreply.github.com>
2022-03-25 12:05:21 -0400

    Create Revert.py

---
+++ Revert.py
@@ -0,0 +1,5 @@
+def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
