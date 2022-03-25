a433f968f3888dffba0a7e3a28c7daaa95fdec03
Hadi Keramati <37190613+hkeramat@users.noreply.github.com>
2022-03-25 11:38:49 -0400

    Create transpose.py

---
+++ transpose.py
@@ -0,0 +1,5 @@
+def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
